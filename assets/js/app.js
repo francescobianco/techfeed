1757bd2a100284ee7c5431204e422906911e73f6
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-04-25 10:45:00 +0200

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,65 @@
-var tag = document.createElement('script');
+(function($){
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady
+
+    loadScripts()
+
+    $(document).ready(function(){
+        inbox()
+    })
+
+    function loadScripts() {
+        var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
+
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'YF08uKbBUFo',
-        playerVars: {
-            autoplay: 1
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
+    var player;
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            height: '390',
+            width: '640',
+            videoId: 'YF08uKbBUFo',
+            playerVars: {
+                autoplay: 1
+            },
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
 
 // 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
 
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
+    var done = false;
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+            setTimeout(stopVideo, 6000);
+            done = true;
+        }
     }
-}
-function stopVideo() {
-    player.stopVideo();
-}
+    function stopVideo() {
+        player.stopVideo();
+    }
+
+
+    function inbox() {
+        $.get('feed/inbox.xml', function(xml){
+            $(xml).find('item').each(function() {
+                var title = $('title', this).text()
+                console.log(title)
+            })
+        })
+    }
+})($);
