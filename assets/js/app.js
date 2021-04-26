d94ad8427e6c80460d574cb29a783369c41fa20c
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-04-27 01:32:50 +0200

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,57 +1,87 @@
 (function($){
+    var player;
+
     $.getScript('https://www.youtube.com/iframe_api');
     window.onYouTubeIframeAPIReady = function() {
         player = new YT.Player('player', {
             height: '390',
             width: '640',
-            videoId: 'YF08uKbBUFo',
-            /*playerVars: {
-                autoplay: 1
-            },*/
             events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
+                'onReady': loadVideo,
+                'onStateChange': function(event) {
+                    console.log('STATE:', event.data);
+                    if (event.data === YT.PlayerState.ENDED) {
+                        markVideo(player.getVideoUrl())
+                        loadVideo()
+                    }
+                }
             }
         })
     }
 
-
-
-
-    var player;
-
-// 4. The API will call this function when the video player is ready.
     function onPlayerReady(event) {
-        player.loadVideoByUrl("https://youtu.be/bHQqvYy5KYo");
         player.playVideo();
         //inbox()
         //event.target.playVideo();
     }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-    var done = false;
-    function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-            setTimeout(stopVideo, 6000);
-            done = true;
-        }
-    }
-    function stopVideo() {
-        player.stopVideo();
+    function getVideoId(url) {
+        url = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+        return (url[2] !== undefined) ? url[2].split(/[^0-9a-z_\-]/i)[0] : url[0];
     }
 
-
-    function inbox() {
-        $.get('feed/inbox.xml', function(xml){
+    function loadFeed(feed, feeds, callback) {
+        console.log("FEED:", feed)
+        var nextTitle = null
+        var currentVideo = null
+        var history = getHistory()
+        $.get(feed, function(xml) {
             $(xml).find('item').each(function() {
-                var title = $('title', this).text()
                 var link = $('link', this).text()
-                console.log(link)
-
-                //player.loadVideoByUrl(link);
+                console.log("URL:", link)
+                if (!history.includes(getVideoId(link))) {
+                    if (currentVideo === null) {
+                        currentVideo = link
+                        player.loadVideoByUrl(link);
+                    } else if (nextTitle === null) {
+                        nextTitle = $('title', this).text()
+                    }
+                }
             })
+            callback(feeds, currentVideo, nextTitle)
         })
     }
-})($);
+
+    function nextFeed(feeds, currentVideo, nextTitle) {
+        if (feeds.length > 0) {
+            if (nextTitle === null || currentVideo === null) {
+                loadFeed(feeds.shift(), feeds, nextFeed)
+            }
+        } else {
+            console.log("NO FEED");
+        }
+    }
+
+    function loadVideo() {
+        var feeds = ['feed/inbox.xml', 'feed/shuffle.xml'];
+        loadFeed(feeds.shift(), feeds, nextFeed)
+    }
+
+    function getHistory() {
+        if (typeof localStorage.history === 'undefined') {
+            return []
+        }
+        try {
+            var history = JSON.parse(localStorage.getItem('history'))
+            return Array.isArray(history) ? history : []
+        } catch (error) {
+            return [];
+        }
+    }
+
+    function mark(url) {
+        var history = getHistory()
+        history.push(getVideoId(url))
+        localStorage.setItem('history', JSON.stringify(history));
+    }
+})($)
