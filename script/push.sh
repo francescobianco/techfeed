086809ded79486703760c4c99a8063636bc1ebf6
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-05-02 01:52:30 +0200

    first commit

--- script/push.sh
+++ script/push.sh
@@ -2,5 +2,5 @@
 
 curl -sLX PUT https://api.github.com/repos/francescobianco/techfeed/contents/$1 \
   -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token ${{ secrets.GITHUB_TOKEN }}" \
+  -H "Authorization: token $2" \
   -d "{\"message\":\"Update $1\",\"content\":\"$(base64 -w0 $1)\",\"sha\":\"$(git ls-files -s $1 | cut -c8-47)\"}"
